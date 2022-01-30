a075f78871fe30ea0f831878128f00bbee1d4440
vmanicse <vmani.cs17@outlook.com>
2022-01-30 14:51:55 +0530

    cors error fixed

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,12 +1,12 @@
-import React from 'react';
-
 export function ApiService() {
 
 	const url = 'https://api.twitter.com/2/spaces/search';
 
 	async function searchByKeyword(keyword) {
-		const headers = {"Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAKacYgEAAAAA00%2Fiyz%2Fot5Uhlvvxtblb8m7H5Aw%3DGOonXNRlv0ReYOB4uzm9RHuvdNpXZqjW9w5joG9Uq9KhvbnRs0"};
-		const res = await fetch(url+'?query='+keyword, {headers});
+		const headers = {
+			"Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAKacYgEAAAAA00%2Fiyz%2Fot5Uhlvvxtblb8m7H5Aw%3DGOonXNRlv0ReYOB4uzm9RHuvdNpXZqjW9w5joG9Uq9KhvbnRs0"
+		};
+		const res = await fetch('https://cors-anywhere.herokuapp.com/'+url+'?query='+keyword, {headers});
 		const result = res.json();
 		return result;
 	}
