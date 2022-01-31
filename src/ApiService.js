5d7428eee93bed14a9f3cfa7f4cfdba2a276ddc9
vmanicse <vmani.cs17@outlook.com>
2022-02-01 00:42:11 +0530

    url changed

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,8 +1,7 @@
 export function ApiService() {
 
 	const BaseUrl = 'https://space-lookup-backend.herokuapp.com/';
-	//const BaseUrl = 'http://localhost:4200';
-
+	
 	async function searchByKeyword(keyword) {
 		const body = {'q': keyword};
 		const headers = {'Content-Type':'application/json'};
